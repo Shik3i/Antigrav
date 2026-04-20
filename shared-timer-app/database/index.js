c8969aeae03184865cbe23bd11163f5c026ea005
MacBook <macbook@shik3i.net>
2026-04-21 01:35:12 +0200

    refactor: extract timer and room domain logic to timers.js

--- shared-timer-app/database/index.js
+++ shared-timer-app/database/index.js
@@ -2,6 +2,7 @@ const { initializeDatabaseSchema } = require('./schema');
 const logging = require('./logging');
 const users = require('./users');
 const economy = require('./economy');
+const timers = require('./timers');
 const legacy = require('./legacy');
 
 // 1. Initialize the database schema and migrations
@@ -12,5 +13,6 @@ module.exports = {
   ...legacy,
   ...logging,
   ...users,
-  ...economy
+  ...economy,
+  ...timers
 };
