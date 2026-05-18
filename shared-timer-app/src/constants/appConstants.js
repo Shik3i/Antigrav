ba86298c5f3f665d88c67b0647c8d0aff4aa8663
MacBook <macbook@shik3i.net>
2026-05-18 08:16:15 +0200

    Release v2.50.0: Rebranding to KoalaWeb and Browser Extension integration

--- shared-timer-app/src/constants/appConstants.js
+++ shared-timer-app/src/constants/appConstants.js
@@ -1,3 +1,3 @@
 export const DEFAULT_LEAGUES = ['LEC', 'LCS', 'LCK', 'LPL', 'VCT_LOCK_IN', 'VCT_EMEA', 'VCT_AMERICAS', 'VCT_PACIFIC'];
-export const DEFAULT_TITLE = 'KoalaSync';
+export const DEFAULT_TITLE = 'KoalaWeb';
 export const EMPTY_ROOM_TOKENS = { readToken: null, writeToken: null };
